5f30f4e72831af06aabd21a301340eca00ed534e
Ubong99 <61476479+Ubong99@users.noreply.github.com>
2021-01-22 11:59:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# CS 360: More Scheme
+
+Name:
+
+Drexel username:
+
+Put anything else you want us to know here.
+
+## Homework statistics
+
+Please let us know how much time you spent on each problem. You may enter time using any format described [here](https://github.com/wroberts/pytimeparse).
+
+Problem 1:
+
+Problem 2:
+
+Problem 3:
+
+Problem 4:
+
+Problem 5:
---
+++ more-scheme.rkt
@@ -0,0 +1,40 @@
+;;
+;; DO NOT REMOVE THESE TWO LINES
+;;
+#lang racket
+(provide (all-defined-out))
+
+;;
+;; Problem 1
+;;
+
+(define (stream-scan f z l)
+  'not-implemented)
+
+;;
+;; Problem 2
+;;
+
+(define (stream-take-n n s)
+  'not-implemented)
+
+;;
+;; Problem 3
+;;
+
+(define (stream-pair-with f s)
+  'not-implemented)
+
+;;
+;; Problem 4
+;;
+                   
+(define (cycle-streams xs ys)
+  'not-implemented)
+
+;;
+;; Problem 5
+;;
+
+(define (seen x)
+  'not-implemented)
